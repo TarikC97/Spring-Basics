6711eea564a81e04d771fc5e126682dc3c17d3d1
TarikC97 <tarikcuric97@gmail.com>
2024-05-19 03:05:20 +0200

    Scopes

--- src/main/java/com/tarikc/Student.java
+++ src/main/java/com/tarikc/Student.java
@@ -2,6 +2,13 @@ package com.tarikc;
 
 public class Student {
 
+    int age;
+
+    public Student(){
+        System.out.println("Object created!");
+    }
+
+
     public void code(){
         System.out.println("Coding");
     }
