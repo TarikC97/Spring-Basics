d524d2aa95def9fe4191e1abf1aabd92956319d9
TarikC97 <tarikcuric97@gmail.com>
2024-08-02 12:46:10 +0200

    SetterInjection

--- src/main/java/com/tarikc/App.java
+++ src/main/java/com/tarikc/App.java
@@ -11,10 +11,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Student stud =(Student) context.getBean("student");
+//        Student stud =(Student) context.getBean("student");
 //        stud.setAge(21);
-        System.out.println(stud.getAge());
-        stud.code();
+//        System.out.println(stud.getAge());
+//        stud.code();
 
 //        Student stud2 =(Student) context.getBean("student");
 //        System.out.println(stud2.age);
