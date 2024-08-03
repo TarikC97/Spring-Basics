bff2887cfed98c63219eb93a8cfb19552eb3558f
TarikC97 <tarikcuric97@gmail.com>
2024-08-03 14:47:32 +0200

    ObjectCreation

--- src/main/java/com/tarikc/Laptop.java
+++ src/main/java/com/tarikc/Laptop.java
@@ -2,7 +2,7 @@ package com.tarikc;
 
 public class Laptop {
     public Laptop(){
-        System.out.println("Object created");
+        System.out.println("Laptop Object created");
     }
     public void compile(){
         System.out.println("Compile");
